094a64d478086b8fa9961c46d5bd38421020147e
edoardo-goracci <egoracci@rockware.de>
2022-04-28 08:25:50 +0200

    com.adobe.cq.email.core.components.util version increased

--- bundles/core/src/main/java/com/adobe/cq/email/core/components/util/package-info.java
+++ bundles/core/src/main/java/com/adobe/cq/email/core/components/util/package-info.java
@@ -18,7 +18,7 @@
  *      This package defines utility classes exposed by the Adobe Experience Manager Core Email Components Bundle.
  * </p>
  */
-@Version("2.0.0")
+@Version("2.1.0")
 package com.adobe.cq.email.core.components.util;
 
 import org.osgi.annotation.versioning.Version;
