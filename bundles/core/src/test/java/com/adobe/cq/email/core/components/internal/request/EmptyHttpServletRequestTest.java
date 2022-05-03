b3c732af30cb43c1e072059f990b1949dffb6017
edoardo-goracci <egoracci@rockware.de>
2022-05-03 15:04:23 +0200

    Copyright section added to new files

--- bundles/core/src/test/java/com/adobe/cq/email/core/components/internal/request/EmptyHttpServletRequestTest.java
+++ bundles/core/src/test/java/com/adobe/cq/email/core/components/internal/request/EmptyHttpServletRequestTest.java
@@ -1,18 +1,28 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2022 Adobe
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.email.core.components.internal.request;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Objects;
 
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.support.ModifierSupport;
 
-import com.adobe.cq.email.core.components.filters.StylesInlinerFilter;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class EmptyHttpServletRequestTest {
 
