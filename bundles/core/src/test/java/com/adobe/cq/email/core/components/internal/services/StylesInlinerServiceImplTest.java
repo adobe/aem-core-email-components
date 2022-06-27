97fca7698c59017893132071997b9b066ad6b4d3
edoardo-goracci <egoracci@rockware.de>
2022-06-27 09:18:32 +0200

    Incorrect HTML encoding for CSS rules fix

--- bundles/core/src/test/java/com/adobe/cq/email/core/components/internal/services/StylesInlinerServiceImplTest.java
+++ bundles/core/src/test/java/com/adobe/cq/email/core/components/internal/services/StylesInlinerServiceImplTest.java
@@ -22,6 +22,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.json.Json;
 import javax.json.JsonReader;
@@ -31,6 +32,7 @@ import org.apache.sling.engine.SlingRequestProcessor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -43,7 +45,9 @@ import com.day.cq.contentsync.handler.util.RequestResponseFactory;
 
 import static com.adobe.cq.email.core.components.TestFileUtils.INTERNAL_CSS_HTML_FILE_PATH;
 import static com.adobe.cq.email.core.components.TestFileUtils.INTERNAL_CSS_JSON_FILE_PATH;
+import static com.adobe.cq.email.core.components.TestFileUtils.INTERNAL_CSS_WITH_IMMEDIATE_CHILDREN_HTML_FILE_PATH;
 import static com.adobe.cq.email.core.components.TestFileUtils.STYLE_AFTER_PROCESSING_FILE_PATH;
+import static com.adobe.cq.email.core.components.TestFileUtils.STYLE_AFTER_PROCESSING_WITH_IMMEDIATE_CHILDREN_FILE_PATH;
 import static com.adobe.cq.email.core.components.TestFileUtils.compareRemovingNewLinesAndTabs;
 import static com.adobe.cq.email.core.components.TestFileUtils.getFileContent;
 
@@ -74,16 +78,19 @@ class StylesInlinerServiceImplTest {
         Document document = Jsoup.parse(result);
         compareRemovingNewLinesAndTabs(getFileContent(STYLE_AFTER_PROCESSING_FILE_PATH),
                 document.selectFirst(StylesInlinerConstants.STYLE_TAG).getAllElements().get(0).data());
-        checkElements(document, "body", Collections.singletonList("font-family: 'Timmana', 'Gill Sans', sans-serif"));
-        checkElements(document, "h1", Arrays.asList("Margin: 0px", "color: #004488", "font-size: 20px"));
-        checkElements(document, "p", Arrays.asList("Margin: 0px", "color: #004488"));
-        checkElements(document, "img", Arrays.asList("-ms-interpolation-mode: bicubic", "border: 10px solid red"));
-        checkElements(document, "table", Arrays.asList("text-align: center !important", "width: 100%"));
-        checkElements(document, "table td", Collections.singletonList("background-color: #ccc"));
-        checkElements(document, ".blocked", Collections.singletonList("display: block"));
-        checkElements(document, ".footer h3", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"));
-        checkElements(document, "h3.example", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"));
-        checkElements(document, "h3.example2", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"));
+        checkElements(document, "body", Collections.singletonList("font-family: 'Timmana', 'Gill Sans', sans-serif"), Optional.empty());
+        checkElements(document, "h1", Arrays.asList("Margin: 0px", "color: #004488", "font-size: 20px"), Optional.empty());
+        checkElements(document, "p", Arrays.asList("Margin: 0px", "color: #004488"), Optional.empty());
+        checkElements(document, "img", Arrays.asList("-ms-interpolation-mode: bicubic", "border: 10px solid red"), Optional.empty());
+        checkElements(document, "table", Arrays.asList("text-align: center !important", "width: 100%"), Optional.empty());
+        checkElements(document, "table td", Collections.singletonList("background-color: #ccc"), Optional.empty());
+        checkElements(document, ".blocked", Collections.singletonList("display: block"), Optional.empty());
+        checkElements(document, ".footer h3", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"),
+                Optional.empty());
+        checkElements(document, "h3.example", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"),
+                Optional.empty());
+        checkElements(document, "h3.example2", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"),
+                Optional.empty());
     }
 
     @Test
@@ -96,20 +103,55 @@ class StylesInlinerServiceImplTest {
         Document document = Jsoup.parse(html);
         compareRemovingNewLinesAndTabs(getFileContent(STYLE_AFTER_PROCESSING_FILE_PATH),
                 document.selectFirst(StylesInlinerConstants.STYLE_TAG).getAllElements().get(0).data());
-        checkElements(document, "body", Collections.singletonList("font-family: 'Timmana', 'Gill Sans', sans-serif"));
-        checkElements(document, "h1", Arrays.asList("Margin: 0px", "color: #004488", "font-size: 20px"));
-        checkElements(document, "p", Arrays.asList("Margin: 0px", "color: #004488"));
-        checkElements(document, "img", Arrays.asList("-ms-interpolation-mode: bicubic", "border: 10px solid red"));
-        checkElements(document, "table", Arrays.asList("text-align: center !important", "width: 100%"));
-        checkElements(document, "table td", Collections.singletonList("background-color: #ccc"));
-        checkElements(document, ".blocked", Collections.singletonList("display: block"));
-        checkElements(document, ".footer h3", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"));
-        checkElements(document, "h3.example", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"));
-        checkElements(document, "h3.example2", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"));
+        checkElements(document, "body", Collections.singletonList("font-family: 'Timmana', 'Gill Sans', sans-serif"), Optional.empty());
+        checkElements(document, "h1", Arrays.asList("Margin: 0px", "color: #004488", "font-size: 20px"), Optional.empty());
+        checkElements(document, "p", Arrays.asList("Margin: 0px", "color: #004488"), Optional.empty());
+        checkElements(document, "img", Arrays.asList("-ms-interpolation-mode: bicubic", "border: 10px solid red"), Optional.empty());
+        checkElements(document, "table", Arrays.asList("text-align: center !important", "width: 100%"), Optional.empty());
+        checkElements(document, "table td", Collections.singletonList("background-color: #ccc"), Optional.empty());
+        checkElements(document, ".blocked", Collections.singletonList("display: block"), Optional.empty());
+        checkElements(document, ".footer h3", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"),
+                Optional.empty());
+        checkElements(document, "h3.example", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"),
+                Optional.empty());
+        checkElements(document, "h3.example2", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"),
+                Optional.empty());
     }
 
-    private void checkElements(Document document, String cssQuery, List<String> expectedStyles) {
-        for (Element element : document.select(cssQuery)) {
+    @Test
+    void success_CssWithImmediateChildren() throws URISyntaxException, IOException {
+        String result =
+                sut.getHtmlWithInlineStyles(resourceResolver, getFileContent(INTERNAL_CSS_WITH_IMMEDIATE_CHILDREN_HTML_FILE_PATH));
+        Document document = Jsoup.parse(result);
+        compareRemovingNewLinesAndTabs(getFileContent(STYLE_AFTER_PROCESSING_WITH_IMMEDIATE_CHILDREN_FILE_PATH),
+                document.selectFirst(StylesInlinerConstants.STYLE_TAG).getAllElements().get(0).data());
+        checkElements(document, "body", Collections.singletonList(""), Optional.empty());
+        checkElements(document, "h1", Arrays.asList("Margin: 0px", "color: #004488", "font-size: 20px"), Optional.empty());
+        checkElements(document, "p", Arrays.asList("Margin: 0px", "color: #004488"), Optional.empty());
+        checkElements(document, "img", Arrays.asList("-ms-interpolation-mode: bicubic", "border: 10px solid red"), Optional.empty());
+        checkElements(document, "table",
+                Arrays.asList("text-align: center !important", "width: 100%"),
+                Optional.of(0));
+        checkElements(document, "table",
+                Arrays.asList("font-family: 'Timmana', 'Gill Sans', sans-serif", "text-align: center !important", "width: 100%"),
+                Optional.of(1));
+        checkElements(document, "table td", Collections.singletonList("background-color: #ccc"), Optional.empty());
+        checkElements(document, ".blocked", Collections.singletonList("display: block"), Optional.empty());
+        checkElements(document, ".footer h3", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"),
+                Optional.empty());
+        checkElements(document, "h3.example", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"),
+                Optional.empty());
+        checkElements(document, "h3.example2", Arrays.asList("border-bottom-width: 12px", "border: 3px solid green", "color: darkgrey"),
+                Optional.empty());
+    }
+
+    private void checkElements(Document document, String cssQuery, List<String> expectedStyles, Optional<Integer> index) {
+        Elements elements = document.select(cssQuery);
+        for (int i = 0; i < elements.size(); i++) {
+            Element element = elements.get(i);
+            if (index.isPresent() && index.get() != i) {
+                continue;
+            }
             String style = element.attr(StylesInlinerConstants.STYLE_ATTRIBUTE);
             List<String> currentStyles =
                     Arrays.stream(style.split(";")).map(String::trim).sorted().collect(Collectors.toList());
