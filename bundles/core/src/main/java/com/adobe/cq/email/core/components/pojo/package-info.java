cd90b829ef3d01de3d9f3c164c46369771d88307
edoardo-goracci <egoracci@rockware.de>
2022-07-04 00:25:11 +0200

    Package info versions increased

--- bundles/core/src/main/java/com/adobe/cq/email/core/components/pojo/package-info.java
+++ bundles/core/src/main/java/com/adobe/cq/email/core/components/pojo/package-info.java
@@ -18,7 +18,7 @@
  * This package defines utility classes exposed by the Adobe Experience Manager Core Email Components Bundle.
  * </p>
  */
-@Version("2.1.0")
+@Version("2.2.0")
 package com.adobe.cq.email.core.components.pojo;
 
 import org.osgi.annotation.versioning.Version;
