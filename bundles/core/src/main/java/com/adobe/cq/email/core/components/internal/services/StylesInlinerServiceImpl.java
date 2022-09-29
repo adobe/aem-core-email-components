d8af862c294bc3a124f7d2b7866e7c9cc08edb2a
Burkhard Pauli <mail@bpauli.de>
2022-09-29 16:16:21 +0200

    @fix StyleInlinerException if nested rule contains separator

--- bundles/core/src/main/java/com/adobe/cq/email/core/components/internal/services/StylesInlinerServiceImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/email/core/components/internal/services/StylesInlinerServiceImpl.java
@@ -16,7 +16,6 @@
 package com.adobe.cq.email.core.components.internal.services;
 
 import java.io.ByteArrayInputStream;
-import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -24,10 +23,8 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -184,17 +181,20 @@ public class StylesInlinerServiceImpl implements StylesInlinerService {
                 if (childToken.isForceUsage()) {
                     continue;
                 }
-                List<String> childCssSelectors = childToken.getJsoupSelectors();
-                for (String childCssSelector : childCssSelectors) {
-                    Elements selectedElements = doc.select(childCssSelector);
+                for (Iterator<String> childCssSelectorIterator = childToken.getJsoupSelectors().iterator(); childCssSelectorIterator.hasNext();) {
+                    Elements selectedElements = doc.select(childCssSelectorIterator.next());
                     if (selectedElements.isEmpty()) {
-                        iterator.remove();
+                        childCssSelectorIterator.remove();
                     }
+                }
+                if (childToken.getJsoupSelectors().isEmpty()) {
+                    iterator.remove();
                 }
 
             }
             if (!styleToken.getChildTokens().isEmpty()) {
                 mediaStyleTokens.add(styleToken);
+
             }
             return;
         }
