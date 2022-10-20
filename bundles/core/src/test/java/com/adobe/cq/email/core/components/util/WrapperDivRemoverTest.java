cc7d7dab429ef5582fce20de14e7d94cb595ae4e
Dirk Rudolph <drudolph@adobe.com>
2022-10-20 15:34:03 +0200

    fix: div removal fails with nested structures and siblings (#236)
    * fix: div remover fails to remove nested siblings
    * chore: remove duplicated dependency
    * fix: organise imports
    * fix: remove unused method parameters
    * fix: specific, case sensitive class matching for div removal

--- bundles/core/src/test/java/com/adobe/cq/email/core/components/util/WrapperDivRemoverTest.java
+++ bundles/core/src/test/java/com/adobe/cq/email/core/components/util/WrapperDivRemoverTest.java
@@ -54,4 +54,22 @@ class WrapperDivRemoverTest {
         compareRemovingNewLinesAndTabs(TestFileUtils.getFileContent(TestFileUtils.WRAPPER_DIV_REMOVAL_OUTPUT_DIVS_REMOVED_FILE_PATH),
                 document.outerHtml());
     }
+
+    @Test
+    void removeDivsSiblings() throws URISyntaxException, IOException {
+        String html = TestFileUtils.getFileContent("wrapper-div-removal/input_remove_siblings_issue.html");
+        Document document = Jsoup.parse(html);
+        WrapperDivRemover.removeWrapperDivs(document, new String[]{"remove"});
+        compareRemovingNewLinesAndTabs(TestFileUtils.getFileContent("wrapper-div-removal/output_remove_siblings_issue.html"),
+            document.outerHtml());
+    }
+
+    @Test
+    void removeDivsClassMatching() throws URISyntaxException, IOException {
+        String html = TestFileUtils.getFileContent("wrapper-div-removal/input_class_matching.html");
+        Document document = Jsoup.parse(html);
+        WrapperDivRemover.removeWrapperDivs(document, new String[]{"classA"});
+        compareRemovingNewLinesAndTabs(TestFileUtils.getFileContent("wrapper-div-removal/output_class_matching.html"),
+            document.outerHtml());
+    }
 }
